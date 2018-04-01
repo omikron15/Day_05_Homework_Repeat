1f7ed3b4a74fe0f2b0b30c3881624e51a8ebb68b
omikron15 <omikron15@hotmail.co.uk>
2018-04-01 15:29:24 +0100

    Day 2 - 7 Tests past

---
+++ pet_shop/pet_shop.rb
@@ -0,0 +1,23 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, num_sold)
+  pet_shop[:admin][:pets_sold] += num_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
